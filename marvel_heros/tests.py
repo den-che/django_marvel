499f2d5bb7f6ec6b5879b607a3b7fe6db0d2bc6b
Denis Cherednikov <denis.cherednikov@gmail.com>
2017-12-19 23:19:49 +0300

    change js and test file

--- marvel_heros/tests.py
+++ marvel_heros/tests.py
@@ -3,7 +3,7 @@ from rest_framework.test import APITestCase, APIClient
 from marvel_heros.models import Hero
 
 class MarvelTest(APITestCase):
-    def check_marvel_api_response(self):
+    def test_check_marvel_api_response(self):
         client = APIClient()
         data = {'name':'DeadPool',
                 'img_link':'test.img',
@@ -11,7 +11,8 @@ class MarvelTest(APITestCase):
                 }
         url = 'api/hero'
         hero_response = self.client.post(url, data, format='json')
-        self.assertEqual(hero_response.status_code, status.HTTP_201_CREATED)
+        print(hero_response)
+        self.assertEqual(hero_response.status_code, 201)
         self.assertEqual(Hero.objects.count(), 1)
         self.assertEqual(Hero.objects.get().name, 'DeadPool')
         
