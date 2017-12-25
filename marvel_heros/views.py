71e7b4a5b7fe2f31e0dd26d28d08bc1dcca693d9
Denis Cherednikov <denis.cherednikov@gmail.com>
2017-12-25 09:45:44 +0300

    Add gitignore

--- marvel_heros/views.py
+++ marvel_heros/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-
 from django.shortcuts import render_to_response
 from rest_framework.viewsets import ModelViewSet
 from marvel_heros.models import Hero
-
 from marvel_heros.serializers import HeroSerializer
+
 
 def get_hero(request):
     heros = Hero.objects.all()
     print(heros)
     return render_to_response('marvel_heros.html',{'heros': heros})
+
 
 def insert_hero(request):
     hero_name = request.GET['data']['results'][0]['name']
@@ -17,6 +17,7 @@ def insert_hero(request):
     hero_description = request.GET['data']['results'][0]['description']
     hero = Hero(name = hero_name, img_link = hero_img_link, description = hero_description)
     hero.save()
+
 
 class HeroViewSet(ModelViewSet):
     queryset = Hero.objects.all()
